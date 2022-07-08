71be5e39490245dc377fafb8c81d6a12efa62087
DarinaChe <108360700+DarinaChe@users.noreply.github.com>
2022-07-08 22:20:14 +0300

    Update Triangle.py

--- Geometricfigure/Triangle.py
+++ Geometricfigure/Triangle.py
@@ -1,13 +1,13 @@
 
 def triangle_p(x, y, z):
-    if x <= 0 or y <= 0 or z <= 0:
+    if x <= 0 or y <= 0 or z <= 0 or (x + y) < z or (x +z ) < y or (z +y) < x:
         return "Нет такой фигуры"
     else:
         return x + y + z
 
 import math
 def triangle_s(x, y, z):
-    if x <= 0 or y <= 0 or z <= 0:
+    if x <= 0 or y <= 0 or z <= 0 or (x + y) < z or (x +z ) < y or (z +y) < x:
         return "Нет такой фигуры"
     else:
         p = (x + y + z)/2
