afd05585ff4b210bc3e43e827d6e5c19f3f571af
darina <darina86@inbox.ru>
2022-07-22 19:31:44 +0300

    Китайский календарь

---
+++ Zadachi8.py
@@ -0,0 +1,34 @@
+a = int(input('Введите год:'))
+
+cycle = 12
+if a % cycle == 0:
+    print ("Год Обезьяны")
+elif (a-1) % cycle == 0:
+    print("Год Петуха")
+elif (a - 2) % cycle == 0:
+    print("Год Собаки")
+elif (a - 3) % cycle == 0:
+    print("Год Свиньи")
+elif (a - 4) % cycle == 0:
+    print("Год Крысы")
+elif (a - 5) % cycle == 0:
+    print("Год Быка")
+elif (a - 6) % cycle == 0:
+    print("Год Тигра")
+elif (a - 7) % cycle == 0:
+    print("Год Кролика")
+elif (a - 8) % cycle == 0:
+    print("Год Дракона")
+elif (a - 9) % cycle == 0:
+    print("Год Змеи")
+elif (a - 10) % cycle == 0:
+    print("Год Лошади")
+elif (a - 11) % cycle == 0:
+    print("Год Коза")
+elif (a - 12) % cycle == 0:
+    print("Год Обезьяны")
+
+
+
+
+
