12e738df93b834b4ca403394c88b588da9b88337
darina <darina86@inbox.ru>
2022-07-26 20:34:49 +0300

    Урок  9

--- Lesson9/Leeson9.py
+++ Lesson9/Leeson9.py
@@ -1,4 +1,4 @@
-#
+# Task 1
 # a = input().split()
 # for i, item in enumerate(a):
 #     a[i] = int(item)
@@ -11,12 +11,28 @@
 #         a[i] = maximum
 # print(a)
 
-list_1 = input()
-list_2 = input()
-if set(list_1).issubset(list_2):
-    print ('True')
-else:
-    print('False')
+# Task 2
+# list_1 = input()
+# list_2 = input()
+# set(list_1).issubset(list_2)
+
+# Task 3
+# def all_subsets(data):
+#     subsets = [[]]
+#     for i in range(len(data)):
+#         for j in range(len(subsets)):
+#             new_subset = subsets[j] + [data[i]]
+#             subsets = subsets + [new_subset]
+#     return subsets
+#
+# print(all_subsets([1, 2 ,3]))
+# print(set([1, 2, 3]).issubset(set([1, 3])))
+
+# itertools - для работы с множествами и подмножествами
 
 
+a = [0, 2, 3, 4]
+x = [3, 5, 7, 9]
 
+dict1 = dict(zip(a, x))
+print(dict1)
