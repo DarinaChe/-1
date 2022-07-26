3cbbf25ef418a4b9d31386f20227e0689b8a0b1d
darina <darina86@inbox.ru>
2022-07-26 20:08:20 +0300

    Урок 9

--- Lesson9/Leeson9.py
+++ Lesson9/Leeson9.py
@@ -1,14 +1,22 @@
+#
+# a = input().split()
+# for i, item in enumerate(a):
+#     a[i] = int(item)
+# maximum = max(a)
+# minimum = min(a)
+# for i in range(len(a)):
+#     if a[i] == maximum:
+#         a[i] = minimum
+#     elif a[i] == minimum:
+#         a[i] = maximum
+# print(a)
 
-a = input().split()
-for i, item in enumerate(a):
-    a[i] = int(item)
-maximum = max(a)
-minimum = min(a)
-for i in range(len(a)):
-    if a[i] == maximum:
-        a[i] = minimum
-    elif a[i] == minimum:
-        a[i] = maximum
-print(a)
+list_1 = input()
+list_2 = input()
+if set(list_1).issubset(list_2):
+    print ('True')
+else:
+    print('False')
+
 
 
